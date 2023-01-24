f6bb80ed3efd27aaac0a1e736b1c8987420880f7
José Cambronero <jcambronero@microsoft.com>
2023-01-23 23:01:01 -0600

    fix install

--- install.sh
+++ install.sh
@@ -1,16 +1,20 @@
 
 # setting up Rust for grmtools
+echo "Setting up Rust"
 curl https://sh.rustup.rs -sSf | bash -s -- -y
 source "$HOME/.cargo/env"
 
 # install grmtools
+echo "Setting up grmtools"
 pushd resources/
-git clone git@github.com:softdevteam/grmtools.git
+git clone https://github.com/softdevteam/grmtools.git
 pushd grmtools
 cargo build --release
 export PATH=$PATH:$(readlink -f target/release)
 popd
+popd
 
 # install our package
+echo "Setting up ai4code_repair"
 pip install -r requirements.txt
 pip install -e .
